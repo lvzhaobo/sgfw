36326b0bb0def2ff0189de2f8f2c1985b9316600
Lv Zhaobo <lvzhaobo@gmail.com>
2014-12-19 01:27:58 +0800

    Refactor Project and Team pages

--- project.php
+++ project.php
@@ -4,9 +4,10 @@
     <style type="text/css">
 	  p {text-indent:2em;line-height:1.6em;font-size:13px;color:#000000;margin:5px 0;border-top:1px solid #F0F099;}
 	  a {color:#FFFFFF;text-decoration:none;}
-	  .project_item {width:206px;height:188px;margin:5px 0px 0px 10px;float:left;padding:10px;border:2px solid #E2E2E2;color:#000000;}
-	  .project_item_title {margin:20px 0 0 20px;font-size:16px;font-weight:bold;color:#FF6600;}
+	  .project_item {width:286px;min-height:220px;margin:5px 0px 0px 10px;float:left;padding:10px;border:2px solid #E2E2E2;color:#000000;}
+	  .project_item_title {margin:20px 0 0 0px;font-size:16px;font-weight:bold;color:#FF6600;line-height:28px;}
 	  .project_item hr {height:1px;display:none;}
+	  .label_part {background-color:#4EB9ED;padding:1px 2px;margin:0 2px;line-height:26px;}
 	</style>
 	<script src="account.js" type="text/javascript"></script>
   <head>
@@ -33,28 +34,44 @@
 			  <hr style="color:#CCCCCC;" />
 			  <br />
 			  
-			  <a href="website.php">
-			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
-			    <span class="project_item_title" style="">校园论坛</span><hr />
-				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
-				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：8" style="margin-left:10px;font-size:20px;color:#FF0000;">8</span></div>
-				<span style="font-size:12px;">技术：HTML、PHP、MySQL、JQuery、GitHub</span>
-				<p>建立一个属于自己学校校园的论坛。整合社团、班级、宿舍、课外活动等多方资源的综合性交流平台。</p>
-			  </div>
+			  <a href="#">
+			    <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
+			      <div style="width:100%;">
+				    <div style="width:140px;float:left;">
+				      <img src="bg.jpg" style="width:132px;"/>
+				    </div>
+				    <div style="width:140px;float:left;margin:5px 0;">
+				      <span class="project_item_title" style="">校园论坛</span><hr />
+				      <div style="margin-top:5px;">
+					    <span style="font-weight:bold;">梦想科技</span><br />
+				        <span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：8" style="margin-left:10px;font-size:20px;color:#FF0000;">8</span>
+				      </div>
+				    </div>
+				  </div>
+			      <div style="clear:float;clear:both;"></div>
+				  <div style="">
+				    <span style="font-size:12px;"><span class="label_part">HTML</span> 
+				    <span class="label_part">PHP</span> 
+				    <span class="label_part">MySQL</span> 
+				    <span class="label_part">JQuery</span> 
+				    <span class="label_part">GitHub</span>
+				  </div>
+				  <p>建立一个属于自己学校校园的论坛。整合社团、班级、宿舍、课外活动等多方资源的综合性交流平台。</p>
+			    </div>
 			  </a>
 			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
 			    <span class="project_item_title" style="">校园周边服务网</span>
 				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
 				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：28" style="margin-left:10px;font-size:20px;color:#FF0000;">28</span></div>
-				<span style="font-size:12px;">技术：HTML、PHP、MySQL、JQuery、GitHub</span>
+				<span style="font-size:12px;">HTML、PHP、MySQL、JQuery、GitHub</span>
 				<p>学校周边肯定有不少商店、餐馆、书店，把这些资源整合到网上，会很方便的，否则你都不知道在哪里配钥匙。</p>
 			  </div>
-			  <a href="database.php">
+			  <a href="#">
 			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
 			    <span class="project_item_title" style="">音乐播放器</span>
 				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
 				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：12" style="margin-left:10px;font-size:20px;color:#FF0000;">12</span></div>
-				<span style="font-size:12px;">技术：HTML、PHP、MySQL、JQuery、GitHub</span>
+				<span style="font-size:12px;">HTML、PHP、MySQL、JQuery、GitHub</span>
 				<p>打造班级/宿舍专属音乐播放器，支持音乐推荐、评论、点赞、在线下载等。</p>
 			  </div>
 			  </a>
@@ -62,28 +79,28 @@
 			    <span class="project_item_title" style="">情侣空间</span>
 				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
 				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：14" style="margin-left:10px;font-size:20px;color:#FF0000;">14</span></div>
-				<span style="font-size:12px;">技术：HTML、PHP、MySQL、JQuery、GitHub</span>
+				<span style="font-size:12px;">HTML、PHP、MySQL、JQuery、GitHub</span>
 				<p>现在已经有一些情侣空间的网站，但做好一个和学校紧密结合的情侣空间，还没有哦。</p>
 			  </div>
 			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
 			    <span class="project_item_title" style="">社团官方网站</span>
 				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
 				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：8" style="margin-left:10px;font-size:20px;color:#FF0000;">8</span></div>
-				<span style="font-size:12px;">技术：HTML、CSS</span>
+				<span style="font-size:12px;">HTML、CSS</span>
 				<p>学校有那么多社团，为社团做个网站是个不错的主意。</p>
 			  </div>
 			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
 			    <span class="project_item_title" style="">校园云网盘</span>
 				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
 				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：6" style="margin-left:10px;font-size:20px;color:#FF0000;">6</span></div>
-				<span style="font-size:12px;">技术：HTML、PHP、MySQL、JQuery、GitHub</span>
+				<span style="font-size:12px;">HTML、PHP、MySQL、JQuery、GitHub</span>
 				<p>打造属于自己学校的网盘及云存储。</p>
 			  </div>
 			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
 			    <span class="project_item_title" style="">学校课程在线</span>
 				<div style="margin-top:5px;">开发者：<span style="font-weight:bold;">梦想科技</span>&nbsp;&nbsp;
 				<br /><span style="font-size:12px;">DS-0001</span>&nbsp;<span style="color:#0099FF;">第一周</span>&nbsp;&nbsp;<span title="喜欢：6" style="margin-left:10px;font-size:20px;color:#FF0000;">6</span></div>
-				<span style="font-size:12px;">技术：HTML、PHP、MySQL、JQuery、GitHub</span>
+				<span style="font-size:12px;">HTML、PHP、MySQL、JQuery、GitHub</span>
 				<p>利用学校的一些资源，制作一些学习视频，放在网上，供有需要的同学使用。</p>
 			  </div>
 			  <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
@@ -92,7 +109,7 @@
 			  </div>
 			  <div style="clear:float;clear:both;"></div>
 			</div>
-			
+			<br />
 		  </div>
 		</div>
 	</div>
--- team.php
+++ team.php
@@ -2,8 +2,12 @@
   <head>
     <title>让梦想在时光中飞舞·推动高校学生梦想实践的平台</title>
     <style type="text/css">
-	  p {text-indent:2em;line-height:1.6em;}
+	  p {text-indent:2em;line-height:1.6em;font-size:13px;color:#000000;margin:5px 0;border-top:1px solid #F0F099;}
 	  a {color:#FFFFFF;text-decoration:none;}
+	  .project_item {width:286px;min-height:220px;margin:5px 0px 0px 10px;float:left;padding:10px;border:2px solid #E2E2E2;color:#000000;}
+	  .project_item_title {margin:20px 0 0 0px;font-size:16px;font-weight:bold;color:#FF6600;line-height:28px;}
+	  .project_item hr {height:1px;display:none;}
+	  .label_part {background-color:#4EB9ED;padding:1px 2px;margin:0 2px;line-height:26px;}
 	</style>
 	<script src="account.js" type="text/javascript"></script>
   <head>
@@ -21,13 +25,37 @@
 				  点击这里，加入我们
 				</div>
 			  </a>
-			  <a href="website.php">
-			  <div style="width:210px;height:128px;background-color:#00A7FF;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#00A7FF';">
+			  <div>
+			  <div style="width:720px;float:left;">
+			  <!--<a href="website.php">
+			  <div style="width:210px;min-height:128px;background-color:#00A7FF;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#00A7FF';">
 			    <span style="margin:20px 0 0 20px;font-size:18px;font-weight:bold;">郑州大学（新校区）</span><hr />
-				<p onmouseover="this.style.fontWeight='bold';" onmouseout="this.style.fontWeight='normal';"></p>
+				<div style="font-size:48px;text-align:center;" onmouseover="this.style.fontWeight='bold';" onmouseout="this.style.fontWeight='normal';">8</div>
+				<div style=""><img src="image/comment.jpg" style="height:40px;"/><span style="font-size:20px;">10</span><img src="image/like.jpg" style="height:40px;"/><span style="font-size:20px;">20</span></div>
+			  </div>
+			  </a>-->
+			  <?php 
+				$colleges = array(
+					array("name"=>"郑州大学（新校区）","color"=>"#00A7FF"),
+					array("name"=>"郑州大学（南校区）","color"=>"#71CE01"),
+					array("name"=>"郑州大学（北校区）","color"=>"#FFB500"),
+					array("name"=>"河南大学","color"=>"#B83AFE"),
+					array("name"=>"河南大学（软件学院）","color"=>"#5883E2"),
+					array("name"=>"黄河科技学院","color"=>"#347ABF"),
+					array("name"=>"郑州轻工业学院","color"=>"#347ABF"),
+					array("name"=>"河南工业大学","color"=>"#00CBAE"),
+				);
+				foreach($colleges as $college){
+			  ?>
+			  <a href="">
+			  <div style="width:210px;min-height:128px;background-color:<?php echo $college["color"]?>;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#00A7FF';">
+			    <span style="margin:20px 0 0 20px;font-size:18px;font-weight:bold;"><?php echo $college["name"]?></span><hr />
+				<div style="font-size:48px;text-align:center;" onmouseover="this.style.fontWeight='bold';" onmouseout="this.style.fontWeight='normal';">8</div>
+				<div style=""><img src="image/comment.jpg" style="height:40px;"/><span style="font-size:20px;">10</span><img src="image/like.jpg" style="height:40px;"/><span style="font-size:20px;">20</span></div>
 			  </div>
 			  </a>
-			  <div style="width:210px;height:128px;background-color:#71CE01;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#71CE01';">
+			  <?php }?>
+			  <!--<div style="width:210px;height:128px;background-color:#71CE01;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#71CE01';">
 			    <span style="margin:20px 0 0 20px;font-size:18px;font-weight:bold;">郑州大学（南校区）</span><hr />
 				<p onmouseover="this.style.fontWeight='bold';" onmouseout="this.style.fontWeight='normal';"></p>
 			  </div>
@@ -56,25 +84,58 @@
 			  <div style="width:210px;height:128px;background-color:#00CBAE;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#00CBAE';">
 			    <span style="margin:20px 0 0 20px;font-size:18px;font-weight:bold;">河南工业大学</span><hr />
 				<p></p>
-			  </div>
-			  <div style="width:210px;height:128px;background-color:#0099FF;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#0099FF';">
+			  </div>-->
+			  <div style="width:210px;height:150px;background-color:#0099FF;margin:10px 0 10px 10px;float:left;color:#FFFFFF;padding:10px;" onmouseover="this.style.backgroundColor='#FF0099';" onmouseout="this.style.backgroundColor='#0099FF';">
 			    <span style="margin:20px 0 0 20px;font-size:18px;font-weight:bold;">静候新学校加入</span><hr />
-				<p></p>
+				<p>想成为你们学校的首位加入我们的成员吗？赶紧联系我们。</p>
+			  </div>
+			  </div>
+			  <div style="width:230px;min-height:548px;margin:10px 0 0 5px;float:left;border:2px solid #0099FF;">
+			  
+			  </div>
 			  </div>
 			</div>
 			<div style="clear:float;clear:both;"></div>
 			<div style="margin:20px 0 40px 0;">
 			  <div style="margin:0 0 0 10px;">
-			    <span style="font-size:28px;font-weight:bold;">参与学校</span><br />
-				<span>每个项目有具体计划，对某个项目感兴趣请加入我们。可在完成一个项目后参与其他项目，所有项目均为公益性质（完全免费）。</span>
+			    <span style="font-size:28px;font-weight:bold;">梦想团队</span><br />
+				<span>想显示在此处？先注册页面，尽可能好的完成课程学习、项目开发，让更多的人认识你。</span>
 			  </div>
 			  <?php
 				include 'db.php';
 				$result = mysql_query("select * from sgfw_user where id>=5 order by id desc");
+				$i = 0;
 				while($item = mysql_fetch_array($result)){
 				//foreach($data as $item){
 			  ?>
-			  <div style="width:420;height:140px;border:1px solid #0099FF;padding:10px;font-size:14px;line-height:22px;float:left;">
+			  <a href="">
+			    <div class="project_item" onmouseover="this.style.border='2px solid #0099FF';" onmouseout="this.style.border='2px solid #E2E2E2';">
+			      <div style="width:100%;">
+				    <div style="width:140px;float:left;">
+					  <?php $img = "bg".($item["id"]%7).".jpg";
+						if($img=="bg0.jpg")
+							$img = "bg.jpg";
+					  ?>
+				      <img src="<?php echo $img;?>" style="width:132px;"/>
+				    </div>
+				    <div style="width:140px;float:left;margin:5px 0;">
+				      <span class="project_item_title" style=""><?php echo base64_decode($item["username"])?></span><hr />
+				      <div style="margin-top:5px;">
+					    <span style="font-weight:bold;"><?php echo base64_decode($item["username"])?></span><br />
+				        <span title="喜欢：8" style="margin-left:10px;font-size:20px;color:#FF0000;">8</span>
+				      </div>
+				    </div>
+				  </div>
+			      <div style="clear:float;clear:both;"></div>
+				  <div style="">
+				    <span style="font-size:12px;"><span class="label_part">标签1</span> 
+				    <span class="label_part">标签2</span>
+				  </div>
+				  <p>（小组口号、小组简介、小组项目介绍等任何信息。）</p>
+			    </div>
+			  </a>
+			  
+			  <!--<div style="width:420;height:140px;border:1px solid #0099FF;padding:10px;font-size:14px;line-height:22px;float:left;">
 			    <div class="team_item_title" style="font-weight:bold;color:#FF9900;font-size:18px;line-height:40px;">
 				  <?php echo base64_decode($item["username"])?>
 				</div>
@@ -84,9 +145,10 @@
 				<div style="">
 				  （小组口号、小组简介、小组项目介绍等任何信息。）
 				</div>
-			  </div>
+			  </div>-->
 			  <?php }?>
-			  <div style="width:420;height:140px;border:1px solid #0099FF;padding:10px;font-size:14px;line-height:22px;float:left;">
+			  <div style="clear:float;clear:both;"></div>
+			  <!--<div style="width:420;height:140px;border:1px solid #0099FF;padding:10px;font-size:14px;line-height:22px;float:left;">
 			    <div class="team_item_title" style="font-weight:bold;color:#FF9900;font-size:18px;line-height:40px;">
 				  梦想之星
 				</div>
@@ -96,7 +158,7 @@
 				<div style="">
 				  音乐播放器，在首页中显示本地音乐，包含用户喜欢指数、推荐指数等，还有歌曲搜索框方便添加新的音乐。本软件适用于班级或团体，用于推荐自己喜欢的音乐。
 				</div>
-			  </div>
+			  </div>-->
 			  <div style="clear:float;clear:both;"></div>
 			</div>
 		  </div>
