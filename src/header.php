b59fa48f7d4304deadb98fbe2d2e25c8b1c98b6e
Lv Zhaobo <lvzhaobo@gmail.com>
2015-01-10 00:42:26 +0800

    Refactor study page

--- src/header.php
+++ src/header.php
@@ -34,15 +34,15 @@ $header = <<<EOF
     <style type="text/css">
 	  p {text-indent:2em;line-height:1.6em;}
 	  a {text-decoration:none;color:#5D5D5D;font-weight:bold;}
-	  .nav_item {margin:0 16px;}
-	  .selected,.nav_item:hover{color:#FF9900;border-bottom:2px solid #FF9900;}
+	  .nav_item {padding:0 16px;}
+	  .selected,.nav_item:hover{height:40px;color:#FF9900;border-bottom:2px solid #FF9900;}
 	</style>
-	<div id="logo" style="margin-top:-28px;overflow:hidden;">
+	<div id="logo" style="margin-top:-28px;overflow:hidden;background-color:#F3F3F3;">
 	  <div style="margin:4px 200px 0 0;float:right;font-weight:bold;">
 	    $str
 	  </div>
 	  <div style="clear:float;clear:both;"></div>
-	  <div style="height:48px;border-bottom:2px solid #FF9900;">
+	  <div style="height:48px;border-bottom:2px solid #0099FF;">
 	  <div style="margin:-20px 0 0 200px;float:left;">
 	    <span style="font-size:28px;font-weight:bold;color:#FF9900;">梦&nbsp;&nbsp;想&nbsp;&nbsp;之&nbsp;&nbsp;星</span><br />
 		<!--<span style="color:#FF9900;font-size:14px;">让&nbsp;&nbsp;梦&nbsp;&nbsp;想&nbsp;&nbsp;在&nbsp;&nbsp;时&nbsp;&nbsp;光&nbsp;&nbsp;中&nbsp;&nbsp;飞&nbsp;&nbsp;舞</span><br />-->
--- study.php
+++ study.php
@@ -4,11 +4,11 @@
     <style type="text/css">
 	  p {text-indent:2em;line-height:1.6em;font-size:14px;color:#000000;}
 	  a {color:#FFFFFF;text-decoration:none;}
-	  .project_item {width:206px;height:200px;margin:20px 0px 0px 20px;float:left;color:#FF6600;border:2px solid #F3F3F3;box-shadow:#DCDCDC;}
-	  .project_item:hover {border:2px solid #FF9900;}
-	  .project_item_title {margin:5px 0;font-size:14px;font-weight:weight;color:#333333;text-align:center;}
+	  .project_item {width:206px;height:240px;margin:20px 0px 0px 20px;float:left;color:#333333;border:2px solid #FFFFFF;box-shadow:0 0 0px #DCDCDC;}
+	  .project_item:hover {border:2px solid #FF9900;color:#FF6600;}
+	  .project_item_title {margin:10px 0;font-size:14px;font-weight:weight;text-align:center;}
 	  .project_item hr {color:#FFFFFF;height:1px;display:none;}
-	  .text {margin:5px;font-size:12px;color:#333333;font-weight:normal;}
+	  .text {margin:5px;font-size:12px;font-weight:normal;}
 	</style>
 	<script src="account.js" type="text/javascript"></script>
   <head>
@@ -16,21 +16,14 @@
     <?php include 'src/header.php'?>
 	<div>
 	  <div class="main_index" style="width:960px;margin:0 auto;font-size:14px;">
-	    <div style="width:100%;margin:40px 0 40px 0;min-height:600px;">
+	    <div style="width:100%;margin:20px 0 40px 0;min-height:814px;">
 			  <div style="margin:0 0 0 0px;height:48px;border-bottom:2px solid #F3F3F3;">
-			    <div style="float:left;width:600px;text-align:middle;padding:10px 0;">
-			      <span style="font-size:22px;font-weight:bold;">学习课程</span>
+			    <div style="float:left;text-align:middle;padding:10px 20px;color:#333333;">
+			      <span style="font-size:22px;font-weight:bold;margin:0 5px;">学习课程</span>
 				  <span>每个项目都有指导老师按照计划安排学习。所有项目均为公益性质（完全免费）。</span>
 				</div>
-				<div style="float:right;width:334px;height:40px;">
-				  <a href="register.php">
-				    <div style="margin:5px 0 5px 10px;background-color:#FFFFFF;font-size:22px;font-weight:bold;text-align:center;padding:5px 0;color:#0099FF;border:2px solid #FF9900;">
-				      点击这里，加入我们
-				    </div>
-			      </a>
-				</div>
-				<div style="clear:float;clear:both;"></div>
 			  </div>
+			  <div style="clear:float;clear:both;"></div>
 			  <?php 
 			      $data = array(
 								array("name"=>"HTML CSS","url"=>"website.php","img"=>"image/html.jpg","text"=>"开发静态网站，满足院系、社团、兴趣小组、中小企业官方网站的需求。"),
@@ -39,24 +32,26 @@
 								array("name"=>"ACM","url"=>"","img"=>"image/acm.jpg","text"=>"（敬请期待）不必羡慕ACMer，你也可以成为他们，从这里开始。"),
 								array("name"=>"JavaScript","url"=>"","img"=>"image/jquery.jpg","text"=>"在前端页面处理中的王者语言。"),
 								array("name"=>"Java（JSP）","url"=>"","img"=>"image/java.png","text"=>"高大上的综合性开发语言。"),
-								array("name"=>"软件测试","url"=>"","img"=>"","text"=>"多方面、多角度掌握软件测试，你掌握的不仅是书本上的知识。"),
-								array("name"=>"计算机网络","url"=>"","img"=>"","text"=>"（敬请期待）在实践中学习互联网互通互联的原理。"),
+								array("name"=>"软件测试","url"=>"","img"=>"image/test.jpg","text"=>"多方面、多角度掌握软件测试，你掌握的不仅是书本上的知识。"),
+								//array("name"=>"计算机网络","url"=>"","img"=>"","text"=>"（敬请期待）在实践中学习互联网互通互联的原理。"),
 								array("name"=>"Linux服务器","url"=>"","img"=>"image/ubuntu.jpg","text"=>"（敬请期待）在Linux系统中搭建各种服务，包括FTP服务器、Samba服务器、WEB服务器等。"),
-								array("name"=>"云存储","url"=>"","img"=>"","text"=>"百度云、Dropbox、Google Drive等云存储服务商提供了丰富的API、SDK，赶紧加入云存储吧。"),
-								array("name"=>"第三方开发","url"=>"","img"=>"","text"=>"百度、Google等公司有开发者平台，开发者可以在这些平台上方便快捷的开发、部署应用。"),
-								array("name"=>"等你来发掘","url"=>"","img"=>"","text"=>"如果你有好的想法，能够帮助自己或别人请联系我们。"),
+								array("name"=>"IOS开发","url"=>"","img"=>"image/ios.jpg","text"=>"IOS开发"),
+								array("name"=>"Android开发","url"=>"","img"=>"image/android.png","text"=>"Andriod开发"),
+								//array("name"=>"云存储","url"=>"","img"=>"","text"=>"百度云、Dropbox、Google Drive等云存储服务商提供了丰富的API、SDK，赶紧加入云存储吧。"),
+								array("name"=>"第三方开发","url"=>"","img"=>"image/devleporplatform.jpg","text"=>"百度、Google等公司有开发者平台，开发者可以在这些平台上方便快捷的开发、部署应用。"),
+								array("name"=>"等你来发掘","url"=>"","img"=>"image/dream.jpg","text"=>"如果你有好的想法，能够帮助自己或别人请联系我们。"),
 				  );
 			  ?>
 			  <?php foreach($data as $item){?>
 			    <a href="<?php echo $item['url']?>">
-			    <div class="project_item" style="box-shadow:0 0 5px #F3F3F3;">
+			    <div class="project_item">
 			      <img src="<?php echo $item['img']?>" style="width:204px;height:128px;border:1px solid #DCDCDC;box-shadow:0 0 5px #DCDCDC;" />
 			      <div class="project_item_title" style=""><?php echo $item["name"]?></div>
 				  <div class="text"><?php echo $item['text']?></div>
 			    </div>
 			  </a>
 			  <?php }?>
-			  <a href="website.php">
+			  <!--<a href="website.php">
 			  <div class="project_item">
 			    <span class="project_item_title" style="">HTML CSS</span><hr />
 				<p>开发静态网站，满足院系、社团、兴趣小组、中小企业官方网站的需求。</p>
@@ -112,7 +107,7 @@
 				<p>如果你有好的想法，能够帮助自己或别人请联系我们。</p>
 			  </div>
 			  <div style="clear:float;clear:both;"></div>
-			</div>
+			</div>-->
 			
 		  </div>
 		</div>
