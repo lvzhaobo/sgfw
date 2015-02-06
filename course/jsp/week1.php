e3d6daf478183b978e4ad594b4c28aa003d00c05
Lv Zhaobo <lvzhaobo@gmail.com>
2015-02-06 17:17:15 +0800

    Add JSP course

---
+++ course/jsp/week1.php
@@ -0,0 +1,72 @@
+﻿<html>
+  <head>
+    <title>让梦想在时光中飞舞·推动高校学生梦想实践的平台</title>
+    <style type="text/css">
+	  p {text-indent:2em;line-height:1.6em;}
+	  a {color:#FFFFFF;text-decoration:none;}
+	  .team a {color:#FF9900;text-decoration:none;margin:10px;}
+	  table {font-size:13px;}
+	  ul {margin:0 0 0 -20px;}
+	  .project_item {background-color:#F2F2F2;padding:0 10px 10px 10px;;line-height:20px;font-size:13px;}
+	  .project_item:hover{background-color:#EEEEEE;}
+	  .intro_title {color:#666666;font-weight:bold;margin:5px 0 0 0;}
+	  .intro_content {color:#999999;}
+	  .current {color:#FF9900;}
+	</style>
+	<script src="account.js" type="text/javascript"></script>
+  <head>
+  <body style="margin:0px;font-family:'Microsoft YaHei',宋体,Arial;font-size:14px;">
+	<div style="padding:20px 0px;line-height:24px;border-bottom:1px dashed #FF9900;margin:0 20px;font-size:14px;">
+		    <div style="min-height:32px;font-weight:bold;font-size:18px;color:#333333;">
+			  第一周学习目标
+			</div>
+		    <ul>
+			  <li>Notepad++安装使用</li>
+			  <li>下载安装XAMPP（Apache、MySQL、PHP、Perl），掌握使用方式</li>
+			  <li>PHP和HTML结合、交互，完成计算器</li>
+			  <li>学习PHP的语法（if/else、echo、var_dump）、变量定义及操作（定义数字/字符串变量，进行数字运算、字符串拼接操作）、$_GET</li>
+			</ul>
+		  </div>
+	  <div style="line-height:24px;padding:20px 0;border-bottom:1px dashed #FF9900;margin:0px 20px;">
+		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
+			  工具准备
+			</div>
+			<div style="">
+			  <ul>
+				<li>使用Notepad++编写代码，也可以使用文本编辑器或其他的编辑器。Notepad++下载地址：<a href="http://www.baidu.com/s?wd=notepad%2B%2B&rsv_spt=1&issp=1&f=8&rsv_bp=0&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&inputT=2395" style="color:#FF9900;font-size:14px;">NotePad++下载</a></li>
+				<li>使用火狐（Firefox）浏览器或谷歌（Chrome）浏览器，不要使用IE、360等</li>
+				<li>下载安装XAMPP，例如安装到D盘，工作目录为htdocs（即D:/xampp/htdocs）。在浏览器中访问http://localhost/可查看页面效果。</li>
+			  </ul>
+			</div>
+		  </div>
+		  <div style="line-height:24px;padding:20px 0;margin:0 20px;border-bottom:1px dashed #FF9900;">
+		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
+			任务要求
+			</div>
+			<div style="">
+			  <img src="../../images/php-1.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
+			  <img src="../../images/php-2.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
+			  <ul style="">
+				<li>创建项目，例如myProject。</li>
+				<li>学习使用PHP的echo、数字运算、字符串拼接知识点，并嵌入HTML代码</li>
+				<li>在项目中新建index.php，输出页面如上图所示。</li>
+				<li>点击“计算”，跳转到新页面，显示计算结果。需要有返回链接。</li>
+				<li>查看<a href="../../resource/index.php" style="color:#FF9900;font-weight:bold;">DEMO</a></li>
+			  </ul>
+			</div>
+		  </div>
+		  <div style="line-height:24px;padding:20px 0;margin:0 20px;">
+		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
+			  技巧
+			</div>
+			<div style="">
+			  <ul style="">
+				<li>PHP的语法和C语言类似，数据来源不再是C语言的scanf，而是网页中的数据。</li>
+				<li>如有疑难点，可以先跳过，等一块内容完成后可以再去搜索查找解决办法。</li>
+				<li>有问题，及时在QQ群中询问，不比因为一点小问题影响大局。</li>
+				<li>和同学保持沟通、交流，可以方便的完成自己的任务，也可以帮助对方完成任务。</li>
+			  </ul>
+			</div>
+		  </div>
+  </body>
+</html>
