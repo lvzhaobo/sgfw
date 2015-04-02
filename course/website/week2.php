0dee465cf1d8708be5671092a24621de2a67c014
Lv Zhaobo <lvzhaobo@gmail.com>
2015-04-02 20:07:22 +0800

    Create website week 2 content

--- course/website/week2.php
+++ course/website/week2.php
@@ -21,53 +21,78 @@
 			  第二周学习目标
 			</div>
 		    <ul>
-			  <li>Notepad++使用</li>
-			  <li>HTML元素（h1、table、a、hr、br、div等标签）</li>
-			  <li>CSS样式（font-size、font-weight、color、background-color、width、height等）</li>
+			  <li>学习网站基本布局</li>
+			  <li>选择自己感兴趣的网站类型并能进行分析</li>
+			  <li>学习界面调试工具</li>
+			  <li>学习网站设计报告</li>
 			</ul>
 		  </div>
-	  <div style="line-height:24px;padding:20px 0;border-bottom:1px dashed #FF9900;margin:0px 20px;">
+	  <div style="line-height:24px;padding:20px 0;border-bottom:1px dashed #FF9900;margin:0px 20px;font-size:15px;">
 		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
-			  工具准备
+			  查找主流网站、分析主流网站布局
 			</div>
+			<img src="../../src/images/courses/website-week2-1.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
 			<div style="">
-			  <ul>
-				<li>使用Notepad++编写代码，也可以使用文本编辑器或其他的编辑器。Notepad++下载地址：<a href="http://www.baidu.com/s?wd=notepad%2B%2B&rsv_spt=1&issp=1&f=8&rsv_bp=0&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&inputT=2395">NotePad++下载</a>
-				<li>使用火狐（Firefox）浏览器或谷歌（Chrome）浏览器，不要使用IE、360等
-				<li>在桌面或D盘或E盘等任意一个地方建立一个文件，命名为“my.html”（要求后缀名为html，文件名可以为“my”或其他）
-				<li>在火狐/谷歌浏览器中打开my.html可以查看效果，刷新会立即更新代码改动效果。使用Notepad++打开my.html可以编辑代码。
-			  </ul>
+				<p>上图为郑州大学信息工程学院网站首页截图，主要分为页面的①header②菜单（menu）③正文（包括N行，第一行左边为图片右边为带有标题的新闻列表，第二行为校内连接、学院公告、教研通知）④footer（页脚，没有显示出来）。Header为100%宽度，正文为900多px，且居中显示，和第一周任务中的说明一致。
+其实每一小部分在第一周任务中基本都已经做过了。复杂的网站也是通过基础知识堆积起来的。先分清层次有助于看清本质。</p>
+			</div>
+			<img src="../../src/images/courses/website-week2-2.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
+			<div style="">
+				<p>上图为清华大学主页上点击学校概况后显示页面中间部分的截图。Header、footer等略去，对应信工院主页的新闻列表，清华大学网站上使用了另一种方式，几大项内容使用图片加文字解释，作用就是把校长致辞、学校沿革、历任领导等内容展示出来（最简单的方式是在左边加个列表，每行显示一项），这种方式就比较美观、直观。在实现难度上，由于有了第一周的基础，不会有太多麻烦。</p>
 			</div>
 		  </div>
-		  <div style="line-height:24px;padding:20px 0;margin:0 20px;border-bottom:1px dashed #FF9900;">
+		  <div style="line-height:24px;padding:20px 0;margin:0 20px;border-bottom:1px dashed #FF9900;font-size:15px;">
 		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
-			任务要求
+			作业及要求
 			</div>
 			<div style="">
-			  <img src="../../week1-1.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
-			  <ul style="">
-				<li>html页面中要包含head和body。在head中设置网页的title，如图中左上角的名字。</li>
-				<li>一般在body中编写html代码，整体上设置宽度为960像素，居中显示。</li>
-				<li>Part 1，使用h1和h4。</li>
-				<li>Part 2，包括标题和表格，建议使用table。标题高28像素，背景颜色为#FF9900;。表格有两行（tr），四列（分别是th、td、th、td）。表格中所有字体靠右显示，边框（border）为1像素。</li>
-				<li>Part 3，标题和Part 2一样，颜色进行修改（自己喜欢的任意颜色即可）。表格中有两行，如Part2，没有边框，其中th的列字体靠右显示，td中的字体不做修改。</li>
-				<li>Part 4，不要使用table，使用div。标题样式如上。评价中有两行，中间需要换行。第一行“勤奋好学”设置为段落。“橙色”字体使用span，且字体颜色需要修改。第二行字体大小设置为28像素，加粗显示。</li>
-				<li>Part 5，标题如上。添加链接，字样显示为“梦想之星英语社”，链接地址为：http://i.my-dreamstar.com，设置链接颜色为#000000;，取消下划线。加一条横线。添加图片，图片来源：http://i.my-dreamstar.com/images/logo.jpg，图片宽度设置为100像素。</li>
+			  <ul style="font-size:14px;">
+			    <li>1.游戏网站，英雄联盟、疯狂的小鸟或消消乐等等游戏的介绍、玩法</li>
+				<li>2.电影/电视剧/动漫，做一个主题网站吧</li>
+				<li>3.美食网站  ，名字可以叫做“谢天谢地饭来了”</li>
+				<li>4.学院篮球队、足球队等兴趣小组网站</li>
+				<li>5.社团网站</li>
+				<li>6.学校门口的书店、小吃店网站</li>
+				<li>7.个人网站，或者情侣空间</li>
+				<li>8.微商等产品宣传网站</li>
+				<li>9.学校失物招领网站</li>
+				<li>10.学校活动、各学院班级课程表网站，方便了解学校动态</li>
 			  </ul>
+			  <p>
+			    和前面分析的步骤类似，参考这10项（可以不限于以上）选择自己喜欢的、认为布局合理的网站（主流风格网站、太复杂的不建议考虑），分析有那几部分组成。需要找1-4个网站（建议2-4个，根据学校课程压力自行调节，也可第一个主要说明，其他简要说明），需要分析首页、以及项目详细页（如在信工院网站上点击新闻后的页面）。不需要太多格式，但是要条理清晰，可以用数字标明多项。
+			  </p>
+			  <p>
+			    作业提交内容为：word文档，需包括①选择的网站类型说明及网址（如团购网站，美团：www.meituan.com，糯米团购：www.nuomi.com）；②对网站进行布局分析的图片；③对网站布局分析的简单理由（建议不超过500字）
+			  </p>
 			</div>
 		  </div>
-		  <div style="line-height:24px;padding:20px 0;margin:0 20px;">
+		  <div style="line-height:24px;padding:20px 0;border-bottom:1px dashed #FF9900;margin:0px 20px;font-size:15px;">
+		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
+			  附加学习
+			</div>
+			梦想之星英语社网址为：http://i.my-dreamstar.com
+			<img src="../../src/images/courses/website-week2-3.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
+			<div style="">
+				<p>下载安装火狐浏览器firebug插件，安装好之后需要重启火狐浏览器以便生效。</p>
+			</div>
+			<img src="../../src/images/courses/website-week2-4.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
+			<div style="">
+				<p>在键盘中按F12或在浏览器的工具->开发者工具中打开firebug，如上图所示。下方会显示出页面代码结构和内容，方便查看。</p>
+			</div>
+			<img src="../../src/images/courses/website-week2-5.jpg" style="width:720px;border:2px solid #CCCCCC;border-radius:0.5em;" />
+			<div style="">
+				<p>点击左边的图标，然后移动到页面的最新消息中（或任意地方），可在firebug面板中查看此处的代码，方便查看任意位置的代码。如想知道苹果官网的效果或腾讯、新浪网站的内容，可以用此方法查看，可以从中学到东西的。</p>
+			</div>
+		  </div>
+		  <div style="line-height:24px;padding:20px 0;margin:0 20px;font-size:14px;">
 		    <div style="font-size:18px;font-weight:bold;color:#333333;margin:10px 0;">
 			  技巧
 			</div>
 			<div style="">
-			  <ul style="">
-				<li>不需要一次性完成，每次编写一块代码，立刻查看效果，进行修改。</li>
-				<li>如有疑难点，可以先跳过，等一块内容完成后可以再去搜索查找解决办法。</li>
-				<li>以上说的效果（如字体大小、颜色、靠右显示等）是附加的，如果没有这些效果，内容也可以显示，只是没有那么好看。这些效果一般是写在style中的（如<div style=””></div>）。这些效果是依靠CSS来实现的，其他部分即为HTML。</li>
-				<li>如有时间，可以在火狐浏览器中安装插件firebug，安装后重启火狐浏览器，按F12打开/关闭。在谷歌浏览器中默认已安装，可直接使用。</li>
-				<li>和同学保持沟通、交流，可以方便的完成自己的任务，也可以帮助对方完成任务。</li>
-			  </ul>
+			  <p>
+			    对于网站开发、软件开发，良好的文档时少不了的，可以方便团队成员很好的交流。考虑到大家时间有限，我们整理了一份设计文档，希望大家能认真阅读并掌握其内容和思路。
+			  </p>
+			  <a href="../../src/doc/website_design_document.doc" style="color:#333333;margin:0 0 0 50px;">点击下载网站设计文档</a>
 			</div>
 		  </div>
   </body>
