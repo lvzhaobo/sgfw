8e3b5f22e32533d8b97dd213959e378fcd881178
Lv Zhaobo <lvzhaobo@gmail.com>
2015-04-17 23:42:17 +0800

    Add website week4

---
+++ course/website/week4.php
@@ -0,0 +1,79 @@
+﻿<html>
+  <head>
+    <title>让梦想在时光中飞舞·推动高校学生梦想实践的平台</title>
+    <style type="text/css">
+	  p {text-indent:2em;line-height:1.6em;}
+	  a {color:#FFFFFF;text-decoration:none;}
+	  .team a {color:#FF9900;text-decoration:none;margin:10px;}
+	  table {font-size:13px;}
+	  ul {margin:0 0 0 -20px;list-style: url;}
+	  .project_item {background-color:#F2F2F2;padding:0 10px 10px 10px;;line-height:20px;font-size:13px;}
+	  .project_item:hover{background-color:#EEEEEE;}
+	  .intro_title {color:#666666;font-weight:bold;margin:5px 0 0 0;}
+	  .intro_content {color:#999999;}
+	  .current {color:#FF9900;}
+	  .item {padding:10px 20px;border-left:2px solid #00AAFF;margin:20px 0px;background-color:#FFFFFF;}
+	  .item_title {font-weight:bold;font-size:18px;line-height:28px;margin:0 0 5px 0;}
+	</style>
+	<script src="account.js" type="text/javascript"></script>
+  <head>
+  <body style="padding:0 10px 0px 10px;font-family:'Microsoft YaHei',宋体,Arial;line-height:24px;color:#333333;font-size:14px;background-color:#EEEEEE;">
+	<div style="" class="item">
+	  <div style="" class="item_title">
+	    第三周学习目标
+	  </div>
+	  <ul>
+	    <li>根据网站规划详细设计网站，构思设计好网站布局、颜色搭配、图片使用等</li>
+	    <li>实现网站基本布局</li>
+	    <li>编写代码并掌握解决未知问题的能力</li>
+		<li>了解软件开发模式</li>
+		<li>了解服务器、域名、及如何部署网站、访问请求流程</li>
+	  </ul>
+	</div>
+	<div style="border-left:2px solid #FFAA00;" class="item">
+	  <div style="" class="item_title">
+	    首页页面
+	  </div>
+	  <div style="">
+		<ul>
+		  <li>header、菜单部分</li>
+		  <li>内容区域（图片列表、新闻列表）</li>
+		  <li>footer部分</li>
+		</ul>
+		<p>相信实现这些内容，仅靠第一周学到的东西还不够，我们也无法把所有的标签、样式讲解一边，但我们也相信你已经掌握了大致思路，通过网上查找具体标签、样式的使用，能够顺利解决这些问题。有任何问题，欢迎随时在QQ群里讨论、交流。不管什么方式，我学到了就赚到了。</p>
+	  </div>
+	</div>
+	<div style="border-left:2px solid #FF0099;" class="item">
+	  <div style="" class="item_title">
+	    内容页页面
+	  </div>
+	  <div style="">
+		<ul>
+		  <li>header、菜单部分</li>
+		  <li>内容区域（左侧菜单、右侧内容）</li>
+		  <li>footer部分</li>
+		</ul>
+		<p>在所有页面中header和footer都是一样的，有什么办法不需要重复Copy呢？<p>
+	  </div>
+	</div>
+	<div style="border-left:2px solid #9900FF;" class="item">
+	  <div style="" class="item_title">
+		附加学习之软件开发模式
+	  </div>
+	  <p>开发模式是用于组织团队一起写作进行软件开发的，包括多种，有敏捷模式、极限编程、瀑布模式等。具体角色包括产品负责人、开发人员、专职测试人员。简单介绍下敏捷模式，有兴趣可以在网上查找敏捷开发或SCRUM。</p>
+	  <p>在敏捷模式中软件不是一次性完成的，把任务进行划分为小任务，然后再细分到几个小时的小任务，每3-5周完成一个迭代（包括几十个小任务），每个迭代有明确的目标，下一个迭代会完成新的目标（任务进行时划分为几十个小任务）。</p>
+	  <p>对于第三周任务，网站是不可能一周做完做好的。先用一周时间完成网站主框架（这一周的所有活动都围绕这个目标进行），下一周逐步完成网站的各个菜单模块并完善页面样式。这两周的时间也不可能做好，如果想做到真正好用的网站，还需要再有几周这样的循环。每一次都是在以前的基础上有所更新和完善。</p>
+	</div>
+	<div style="border-left:2px solid #99FF00;" class="item">
+	  <div style="" class="item_title">
+		文档
+	  </div>
+	  <div style="">
+		<p>
+		  对于网站开发、软件开发，良好的文档时少不了的，可以方便团队成员很好的交流。考虑到大家时间有限，我们整理了一份设计文档，希望大家能认真阅读并掌握其内容和思路。
+		</p>
+		<a href="../../src/doc/website_design_document.docx" target="_blank" style="color:#333333;margin:0 0 0 50px;">点击下载网站设计文档</a>
+	  </div>
+	</div>
+  </body>
+</html>
